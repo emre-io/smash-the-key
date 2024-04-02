c743776d8dce35730d74e046d5ab0ccd60aa60e6
Emre Sahin <e.sahin6677@gmail.com>
2024-04-02 20:08:18 +0200

    push first html layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+
+// Counter 
+// StartButton
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='h-screen flex flex-col justify-center items-center gap-3'>
+          <div className='flex flex-row gap-3'>
+            <div>
+              <p className='bg-sky-500 px-2 py-0.25 my-0.5'> Player 1</p>
+            </div>
+            <div>
+              <p className='bg-sky-500 px-2 py-0.25 my-0.5'> Player 2</p>
+            </div>
+          </div>
+          <div className='container w-40 border-4 '>
+            <div className='flex flex-row gap-3 justify-evenly'>
+              <div>
+                <p className='bg-sky-500 px-2 py-0.25 my-0.5'>Time</p>
+              </div>
+              <div>
+                <p className='bg-sky-500 px-2 py-0.25 my-0.5'>Time</p>
+              </div>
+            </div>
+            <div className='flex flex-row gap-3 justify-evenly'>
+              <div>
+                <p className='bg-sky-500 px-2 py-0.25 my-0.5'>"S"</p>
+              </div>
+              <div>
+                <p className='bg-sky-500 px-2 py-0.25 my-0.5'>"X"</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <button className='bg-sky-500 rounded px-2 py-0.25'> Start </button>
+          </div>  
+          <div>
+            <p className='bg-sky-500 px-2 py-0.25'>Counter</p>
+          </div>
+        </div>
+        
     </div>
   );
 }
