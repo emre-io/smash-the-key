877df42c9f2a5a93557bbe400e2c8663044b4a5b
Emre Sahin <e.sahin6677@gmail.com>
2024-04-14 18:08:56 +0200

    remove timers

--- src/App.js
+++ src/App.js
@@ -9,15 +9,8 @@ function App() {
   const [player1Won, setPlayer1Won] = useState("");
   const [player2Won, setPlayer2Won] = useState("");
 
-  const [startTime1, setStartTime1] = useState(null);
-  const [now1, setNow1] = useState(null);
-  const intervalRef1 = useRef(null);
-
-  const [startTime2, setStartTime2] = useState(null);
-  const [now2, setNow2] = useState(null);
-  const intervalRef2 = useRef(null);
-
-  const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+  //const ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   const useEventListener = (eventName, handler, element = window) => {
     const savedHandler = useRef();
@@ -34,13 +27,12 @@ function App() {
   };
 
   const handler = ({ key }) => {
-    handleStop();
+    //handleStop();
     if (player1RandomChar === String(key)) {
       setPlayer1Won("won");
     } else if (player2RandomChar === String(key)) {
       setPlayer2Won("won");
     }
-    console.log("Random key pressed!");
   };
 
   useEventListener("keydown", handler);
@@ -56,7 +48,7 @@ function App() {
             setPlayer2RandomChar(
               ALPHABET[Math.floor(Math.random() * ALPHABET.length)]
             );
-            handleStart();
+            //handleStart();
             clearInterval(timer);
             return 0;
           } else return time - 1;
@@ -65,41 +57,8 @@ function App() {
     }
   }, [startTimer]);
 
-  function handleStart() {
-    setStartTime1(Date.now());
-    setNow1(Date.now());
-
-    clearInterval(intervalRef1.current);
-    intervalRef1.current = setInterval(() => {
-      setNow1(Date.now());
-    }, 10);
-
-    setStartTime2(Date.now());
-    setNow2(Date.now());
-
-    clearInterval(intervalRef2.current);
-    intervalRef2.current = setInterval(() => {
-      setNow2(Date.now());
-    }, 10);
-  }
-
-  function handleStop() {
-    clearInterval(intervalRef1.current);
-    clearInterval(intervalRef2.current);
-  }
-
   function handleCountdownStart() {
     setStartTimer(1);
-  }
-
-  let secondsPassed1 = 0;
-  if (startTime1 != null && now1 != null) {
-    secondsPassed1 = (now1 - startTime1) / 1000;
-  }
-
-  let secondsPassed2 = 0;
-  if (startTime2 != null && now2 != null) {
-    secondsPassed2 = (now2 - startTime2) / 1000;
   }
 
   return (
@@ -132,28 +91,28 @@ function App() {
               </p>
             </div>
           </div>
-          <div className="flex flex-row gap-3 justify-evenly">
-            <div>
-              <p className="bg-sky-500 px-2 py-0.25 my-0.5">
-                Time passed: {secondsPassed1.toFixed(2)}
-              </p>
-            </div>
-            <div>
-              <p className="bg-sky-500 px-2 py-0.25 my-0.5">
-                Time passed: {secondsPassed2.toFixed(2)}
-              </p>
-            </div>
-          </div>
         </div>
         <div>
-          <p>
-            Time left: {`${Math.floor(time / 60)}`.padStart(2, 0)}:
-            {`${time % 60}`.padStart(2, 0)}
-          </p>
-          <button className="py-0.5 px-1 rounded bg-indigo-500 mx-1" onClick={() => handleCountdownStart()}>
-            Start
-          </button>
-          <button className="py-0.5 px-1 rounded bg-indigo-500" onClick={() => window.location.reload()}>Play Again</button>
+          <div className="flex flex-row gap-3 justify-evenly">
+            <p>
+              Time left: {`${Math.floor(time / 60)}`.padStart(2, 0)}:
+              {`${time % 60}`.padStart(2, 0)}
+            </p>
+          </div>
+          <div className="flex flex-row gap-3 justify-evenly">
+            <button
+              className="py-0.5 px-1 rounded bg-indigo-500 mx-1"
+              onClick={() => handleCountdownStart()}
+            >
+              Start
+            </button>
+            <button
+              className="py-0.5 px-1 rounded bg-indigo-500"
+              onClick={() => window.location.reload()}
+            >
+              Play Again
+            </button>
+          </div>
         </div>
       </div>
     </div>
